3ca9d92c04b6aed5f1771842e16bb632d3dce8bc
owais <owais.niaz@targetofs.com>
2021-01-21 15:36:51 +0500

    Deletion of some Unused FILE

--- crypto/crypto.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto')
-const http = require("http")
-http.createServer((req,res)=>{
-    let hash = crypto.createHash('md5').update('awaisniaz','utf8').digest('hex')
-    res.write(hash)
-    res.end()
-}).listen(3000,()=>{
-    console.log('I am Crypto')
-})
