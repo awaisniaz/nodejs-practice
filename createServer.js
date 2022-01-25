32cccef5de808b83bbd6fd71a35d8fc0732e5d00
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-01-25 22:55:55 +0500

    Update createServer.js

--- createServer.js
+++ createServer.js
@@ -1,6 +1,8 @@
 const http = require('http')
 var host = '127.0.0.1'
 http.createServer((req, res) => {
+    
+    
     res.writeHead(200, { "content-type": "text/plain" })
     res.write('I am Server ')
     res.write('I am Double Server')
